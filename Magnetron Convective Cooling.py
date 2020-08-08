62d8d781699dd8ffc32249216bb430e1ebc5f37b
Theo <theo@theomacklin.com>
2020-08-08 16:34:04 +0100

    Working data import

--- Magnetron Convective Cooling.py
+++ Magnetron Convective Cooling.py
@@ -1,6 +1,22 @@
-import csv
+import csv, numpy as np
 
-with open("./ConvectiveCoolingDesigns.csv", r) as file:
-    CSVlines = csv.DictReader(file)
+dataCSVName = "ConvectiveCoolingDesigns"
 
-print(CSVlines)
+def openCSV(fileName):
+    if fileName[-4:] != ".csv":
+        fileName += ".csv"
+    with open("./" + fileName, "r") as file:
+        propArray = np.array([i for i in csv.reader(file)])[:,:-1]
+
+    propArray[1:], propDict = (np.where(propArray[1:] == "", 0, propArray[1:])), {}
+    for i in range(0,len(propArray[0])):
+        propDict[propArray[0,i].strip()] = propArray[1:,i].astype(np.float)
+    return propDict
+
+def main():
+    global dataCSVName
+    propDict = openCSV(dataCSVName)
+    print(propDict['OptionNumber'])
+    return
+
+main()
